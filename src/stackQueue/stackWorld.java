5c1e2f2049cbdb64b0ecf1185a4ab5c7c13cf4d5
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-25 22:56:41 -0800

    update done

--- src/stackQueue/stackWorld.java
+++ src/stackQueue/stackWorld.java
@@ -1,11 +1,35 @@
 package stackQueue;
+import java.util.Stack;
 
 public class stackWorld {
 
 	public static void main(String[] args) {
 
+       Stack<String> myStack = new Stack<String>();
        
-
-	}
-
-}
+       myStack.push("Student 1");
+       myStack.push("Student 2");
+       myStack.push("Student 3");
+       myStack.push("Student 4");
+       myStack.push("Student 5");
+       myStack.push("Student 6");
+       while(!myStack.empty())
+    		   System.out.println(myStack.pop());
+       System.out.println();
+       rev();
+       
+	}//End of main class
+	
+   public static void rev()
+   {
+	   Stack<Character> revString = new Stack<Character>();
+       String myString = "anwaar";
+       
+       for(int i = 0; i < myString.length(); i++)
+       {
+    	   revString.push(myString.charAt(i));
+       }
+       while(!revString.empty())
+		   System.out.print(revString.pop());
+   }
+}//End of stackWorld class
