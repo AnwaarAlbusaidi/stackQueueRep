4e293425507ec6dc123903e126b413905c45be5f
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-27 08:56:07 -0800

    Done

--- src/stackQueue/stackWorld.java
+++ src/stackQueue/stackWorld.java
@@ -1,4 +1,6 @@
 package stackQueue;
+import java.awt.FocusTraversalPolicy;
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class stackWorld {
@@ -17,7 +19,15 @@ public class stackWorld {
     		   System.out.println(myStack.pop());
        System.out.println();
        rev("something"); //call the method rev to reverse the word something
-       
+       System.out.println();
+       System.out.println(openClose("{{int x=0;}}"));
+       System.out.println(openClose("{{{ }}"));
+       System.out.println(openClose("{"));
+       System.out.println(openClose("{"));
+       System.out.println();
+       revSent("reverse me if you can");
+       System.out.println();
+       System.out.println(spilt("reverse me if you can" , ' '));
 	}//End of main class
 	
    public static void rev(String str)
@@ -31,4 +41,57 @@ public class stackWorld {
        while(!revString.empty())
 		   System.out.print(revString.pop());
    }
+   
+   //This method well return true if all {} are equal and false if not equal
+   public static boolean openClose(String str)
+   {
+	   Stack<Character> openClosePar = new Stack<Character>();
+	   
+	   for(int i = 0; i < str.length(); i++)
+       {
+    	  if(str.charAt(i) == '{')
+    		  openClosePar.push(str.charAt(i));
+    	  else if (str.charAt(i)== '}' )
+    	  {
+    		  if(!openClosePar.empty())
+    	         openClosePar.pop();
+    		  else
+    			  return false;
+    	  }
+       }   
+	   if(openClosePar.empty())
+		   return true;
+	   else
+	      return false;
+   }
+   //this method reverse a string using stack and .split method
+   public static void revSent(String str)
+   {
+	   Stack<String> revString = new Stack<String>();
+       for (String res: str.split(" "))
+       {
+    	   revString.push(res);
+       }
+       while(!revString.empty())
+		   System.out.print(revString.pop() + " ");
+   }
+   
+   //This is a re-implementation of split method 
+   public static ArrayList<String>  spilt(String str , char ch) {
+	   
+	  ArrayList<String> spiltArrayList = new  ArrayList<String>();
+	
+	   String wordString = "";  
+	  for(int i = 0 ; i < str.length() ; i++)
+	  {
+		  wordString = wordString + str.charAt(i);
+		  if(str.charAt(i) == ch || i == str.length()-1)
+		  {
+			  spiltArrayList.add(wordString);
+			  wordString = "";
+		  }
+	  }
+	  return spiltArrayList;
+   }
+   
 }//End of stackWorld class
